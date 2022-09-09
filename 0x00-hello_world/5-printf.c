35ca2e83a7ea7c54466c8bdb0cc675c5f68a6d5f
“Tebogoclive” <clive.kgonothi@gmail.com>
2022-09-08 19:41:02 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
  /**
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "Programming is like a multiligual puzzle
+ * this program prints "Programming is like a multiligual puzzle"
  * Return: 0
  */
 int main(void)
