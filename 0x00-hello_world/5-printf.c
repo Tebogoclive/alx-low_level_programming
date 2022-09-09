f5c6e9c7a8fd7c4dfbf764ce12652d6622c53de0
“Tebogoclive” <clive.kgonothi@gmail.com>
2022-09-08 19:00:07 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
- /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return:(0);
 }
