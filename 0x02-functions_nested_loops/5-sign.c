91b072d7a2046d73616eac6f6607ff94095b121f
“Tebogoclive” <clive.kgonothi@gmail.com>
2022-09-13 11:46:19 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,7 @@
  * print_sign - function to check for a sign of a number
  * @n:  is the int that will use for the argument of the function
  * Return: 0
+ */
 int print_sign(int n)
 {
 	if (n > 0)
@@ -13,6 +14,6 @@ int print_sign(int n)
 	else
 	{
 	_putchar('0');
-	return(0);
+	return (0);
 	}
 }
