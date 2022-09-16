e3581e13be52811e40a1ec6f4cf547de38b1291a
“Tebogoclive” <clive.kgonothi@gmail.com>
2022-09-16 04:50:44 -0700

    Fix:UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-/** 
-* main - main block
-* Description: Print the unix system
-* return: 1
+/**
+* main -prints to the stderr
+* Return: returns 1 on success
 */
 int main(void)
+
 {
-int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
-return (1);
+	char str[] = "- Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "and that piece of art is useful\" %s", str);
+	return (1);
 }
