9634da35e803cb10da6af14b3f7678bd98071dc7
“Tebogoclive” <clive.kgonothi@gmail.com>
2022-09-21 14:43:08 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,23 +6,16 @@
 * @src: The second string
 * Return: Pointer to the final string
 */
+
 char *_strcat(char *dest, char *src)
 {
-	char *p;
+	int index = 0, dest_len = 0;
 
-	p = dest;
+	while (dest[index++])
+		dest_len++;
 
-	while (*p != '\0')
-		p++;
-
-	while (*src != '\0')
-	{
-		*p = *src;
-		p++;
-		scr++;
-	}
-
-	*p = '\0';
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
