29dede5d85ef5b9f6847c734ca181e8267c79554
“Tebogoclive” <clive.kgonothi@gmail.com>
2022-09-21 14:28:21 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,28 @@
 #include "main.h"
 
 /**
-* _strcat - concatenates the string pointed to by @src to
-* the end of the string pointed to by @dest
-* @dest: String that will be appended
-* @src: String to be concatenated upon
-*
-* Retunr: returns pointer to @dest
+* _strcat - Concatenates 2 strings
+* @dest: The final string
+* @src: The second string
+* Return: Pointer to the final string
 */
-
 char *_strcat(char *dest, char *src)
 {
+	char *p;
 
-	int index = 0, dest_len = 0;
+	p = dest;
 
-	while (dest[index++])
-		dest_len++;
+	while (*p != '\0')
+		p++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (*src != '\0')
+	{
+		*p = *src;
+		p++;
+		scr++;
+	}
+
+	*p = '\0';
 
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 /**
-* void prototype()
+* void prototypes()
 * int prototypes()
 * char prototypes()
 */
@@ -20,4 +20,4 @@ char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1,char *n2,char *r,int r_index);
 
-#endif/*MAIN_H*/
+#endif /*MAIN_H*/
