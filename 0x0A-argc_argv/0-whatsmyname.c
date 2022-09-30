7e141760efd72fb7ce8256a3db2b2bf2f2d7e24f
“Tebogoclive” <clive.kgonothi@gmail.com>
2022-09-30 07:48:45 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Prints its name
+* @argc: Arg count
+* @argv: Args
+* Return: 0 on success
+*/
+int main(__attribute__((unused)) inr argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
