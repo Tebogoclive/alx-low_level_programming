5154a45021076738020110f6d72fc42b737417e2
“Tebogoclive” <clive.kgonothi@gmail.com>
2022-10-12 12:21:10 -0700

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
 #include "function_pointers.h"
 
 /**
-* print_name - prints a name
-* @name: The given name
-*f: func to do that
-* Return: Nothing
-*/
-void print_name(char *name, void (*f)char *))
+ * print_name - Prints a name
+ * @name: The given name
+ * @f: func to do that
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
 {
-	if(name != NULL && f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
