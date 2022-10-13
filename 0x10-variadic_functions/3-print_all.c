a40efbba31ea7c0cb3e6435b08915a2cc3d69b6a
“Tebogoclive” <clive.kgonothi@gmail.com>
2022-10-13 10:46:53 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,102 +1,48 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
-* print_c - print a char
-* @c: char to print
-*
-* Return: void
-*/
-void print_c(va_list c)
-{
-	printf("%c", va_arg(c, int));
-}
+ * print_all - function that prints anything.
+ * @format: types of arguments passed to the function.
+ */
 
-/**
-* print_s - Prints a string
-* @s: string to print
-*
-* Return: void
-*/
-void print_s(va_list s)
-{
-	char *str = va_arg(s, char *);
-
-	if (str == NULL)
-		str = "(nil)";
-	printf("%s", str);
-}
-
-/**
-* print_i - Prints an integer
-* @i: int to print
-*
-* Return: void
-*/
-void print_i(va_list i)
-{
-	printf("%d", va_arg(i, int));
-}
-
-/**
-* print_f - Prints a float
-* @f: float to print
-*
-* Return: void
-*/
-void print_f(va_list f)
-{
-	printf("%f", va_arg(f, double));
-}
-
-/**
-* print_all - Prints anything
-* @format: list of aurgument type passed to the function
-*
-* Return: void
-*/
 void print_all(const char * const format, ...)
-{
-	unsigned int i, j;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"f", print_f},
-		{NULL, NULL}
-	};
-	va_list valist;
-	char *separator = "";
 
-	va_start(valist, format);
-	i = 0;
-	while (format && format[i])
+{
+	unsigned int i = 0;
+	char *str;
+	va_list list;
+
+	va_start(list, format);
+	while (format && format[i] != '\0')
 	{
-		j = 0;
-		while (p[j].t != NULL)
+		switch (format[i])
 		{
-			if (*p([j].t) == format[i])
-				prinf("%s", separator);
-				p[j].f(valist);
-				separator = ", ";
+			case 'c':
+				printf("%c", va_arg(list, int));
 				break;
-			}
-			j++;
+			case 'i':
+				printf("%i", va_arg(list, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double));
+				break;
+			case 's':
+				str = va_arg(list, char *);
+				if (str)
+				{
+					printf("%s", str);
+					break;
+				}
+				printf("(nil)");
+				break;
+			default:
+				i++;
+				continue;
 		}
+		if (format[i + 1] != '\0')
+			printf(", ");
 		i++;
 	}
-	va_end(valist);
 	printf("\n");
+	va_end(list);
 }
-
-
-
-
-
-
-
-
-
-
-
